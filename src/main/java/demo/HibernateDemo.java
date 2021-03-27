52e0c6fbae7fff8d4adbf64ea10f735409a5725e
bartek369 <bkfalkiewicz@gmail.com>
2021-03-27 11:13:42 +0100

    Metoda getCurrentSpeed. Działa.

--- src/main/java/demo/HibernateDemo.java
+++ src/main/java/demo/HibernateDemo.java
@@ -1,15 +1,20 @@
 package demo;
 
+import api.PositionAPI;
 import model.*;
 import openNotify.OpenNotifyInformation;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import repository.ISSRepository;
 
+import javax.persistence.EntityManager;
 import java.io.IOException;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.util.List;
 
 public class HibernateDemo {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Astronaut.class)
@@ -20,9 +25,28 @@ public class HibernateDemo {
                 .buildSessionFactory();
 
         ISSRepository issRepository = new ISSRepository(sessionFactory.createEntityManager());
-        issRepository.saveAstronauts(OpenNotifyInformation.getPeopleInSpace());
+        //issRepository.saveAstronauts(OpenNotifyInformation.getPeopleInSpace());
+        PositionAPI api = new PositionAPI();
+
+        int x = 0;
+        while (x < 5) {
+            Position position = new Position(api.getTimestamp(), api.getLongitude(), api.getLatitude());
+            issRepository.add(position);
+            issRepository.savePosition(position);
+            System.out.println("*****************************");
+            System.out.println(position);
+            Thread.sleep(2000);
+            x++;
+        }
+
+        issRepository.getCurrentSpeed();
 
 
 
     }
+
+
+
+
+
 }
--- src/main/java/repository/ISSRepository.java
+++ src/main/java/repository/ISSRepository.java
@@ -14,6 +14,7 @@ public class ISSRepository {
     public ISSRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
     public void saveAstronauts(List<Astronaut> astronautsList){
         EntityTransaction transaction = null;
         try{
@@ -22,6 +23,21 @@ public class ISSRepository {
             for(Astronaut astronaut : astronautsList){
                 entityManager.persist(astronaut);
             }
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction !=null){
+                transaction.rollback();
+            }
+        }
+    }
+
+    public void savePosition(Position position){
+        EntityTransaction transaction = null;
+        try{
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(position);
             transaction.commit();
 
         }catch (Exception e){
@@ -45,26 +61,31 @@ public class ISSRepository {
             System.out.println("Blad pobierania danych dot. pozycji");
             return speed;
         }
+        int index = 0;
         for (Position P: Positions) {
-            if (P.getId().equals(1)) {
+            if (index == 0) {
                 lat2 = P.getLatitude();
                 lng2 = P.getLongitude();
-                t2 = P.getTime().getSecond();
-            } else if (P.getId().equals(2)) {
+                t2 = P.getTime();
+            } else if (index == 1) {
                 lat1 = P.getLatitude();
                 lng1 = P.getLongitude();
-                t1 = P.getTime().getSecond();
+                t1 = P.getTime();
             }
+            index++;
         }
         speed = distanceFromInMeters(lat1,lng1,lat2,lng2)/(t2-t1);
-        System.out.println("Prekość ISS: "+ speed);
+        System.out.println("Prędkość ISS [m/s]: " + speed);
+        System.out.println("Prędkość ISS [km/h]: " + speed * 3.6);
         return speed;
     }
 
 
-
+// co gdy jest null (brak danych w tablicy)?
     public List<Position> getLast2Positions() {
-        return entityManager.createQuery("SELECT * FROM people_in_space ORDER BY id DESC LIMIT 2", Position.class).getResultList();
+        String query = "FROM Position ORDER BY id DESC";
+        return entityManager.createQuery(query, Position.class)
+                .setMaxResults(2).getResultList();
     }
 
     public double distanceFromInMeters(double lat1, double lng1, double lat2, double lng2) {
@@ -77,6 +98,25 @@ public class ISSRepository {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         double dist = earthRadius * c;
         return dist;
+    }
+
+    public Position add(Position position) {
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = this.entityManager.getTransaction();
+            transaction.begin();
+            this.entityManager.persist(position);
+            transaction.commit();
+            return position;
+        } catch (Exception var4) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            var4.printStackTrace();
+            return null;
+        }
     }
 
 
